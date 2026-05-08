21d311c20b8d29c8d1fa845f9ed5afaa771cbb9b
Davide Da Col <davidedacol@MacBook-Pro-di-Alesssio.local>
2026-05-08 11:25:24 +0200

    chore: unify dropdown logic

--- src/ui/dropdown.rs
+++ src/ui/dropdown.rs
@@ -15,95 +15,39 @@ pub fn fill_basic_dropdown(
     explorer_status: Res<ExplorerInfoRes>,
     target_entity: Res<EntityClickRes>,
 ) {
-    for (list, dropd) in lists {
-        // don't run if the user is not looking at an explorer
-        if target_entity.explorer.is_none() 
-            || !matches!(dropd, ListType::BasicList) {
+    let Some(explorer_id) = target_entity.explorer else {
+        return;
+    };
+
+    let planet_id = explorer_status
+        .map
+        .get_current_planet(&explorer_id)
+        .unwrap_or(0);
+
+    let planets_info = orchestrator.orchestrator.get_planets_info();
+    let single_info = planets_info.get_info(planet_id);
+
+    let empty = HashSet::new();
+
+    let resources = single_info
+        .and_then(|p| p.supported_resources.as_ref())
+        .unwrap_or(&empty);
+
+    for (list, dropd) in lists.iter() {
+        if !matches!(dropd, ListType::BasicList) {
             continue;
         }
 
-        //don't fill the tables if the explorer isn't in manual mode
-        // TODO remove this unwrap!
-        let explorer_id = target_entity.explorer.unwrap();
-
-        // clear previous table
-        commands.entity(list).despawn_children();
-
-        let planet_id = explorer_status
-            .map
-            .get_current_planet(&explorer_id)
-            .unwrap_or(0);
-
-        //get the planet's available resources
-        let planets_info = orchestrator.orchestrator.get_planets_info();
-        let single_info = planets_info.get_info(planet_id);
-
-        let resources = match single_info {
-            Some(safe_planets) => {
-                if let Some(x) = &safe_planets.supported_resources{
-                    x
-                } else {
-                    &HashSet::new()
-                }
-            },
-            None => &HashSet::new(),
-        };
-
-        // create the actual UI element
-        commands.entity(list).with_children(|parent| {
-            if !resources.is_empty() {
-                for basic in resources {
-                    parent
-                        .spawn((
-                            Button,
-                            ExpButtonActions::CreateBasic(*basic),
-                            Node {
-                                height: Val::Px(28.0),
-                                padding: UiRect::horizontal(Val::Px(8.0)),
-                                align_items: AlignItems::Center,
-                                ..default()
-                            },
-                            DropdownItem {
-                                planet_id,
-                                explorer_id,
-                            },
-                        ))
-                        .with_children(|item| {
-                            item.spawn((
-                                Text::new(format!("{:?}", basic)),
-                                TextFont {
-                                    font_size: 14.0,
-                                    ..Default::default()
-                                },
-                            ));
-                        });
-                }
-            } else {
-                parent
-                    .spawn((
-                        Button,
-                        Node {
-                            height: Val::Px(28.0),
-                            padding: UiRect::horizontal(Val::Px(8.0)),
-                            align_items: AlignItems::Center,
-                            ..default()
-                        },
-                        DropdownItem {
-                            planet_id,
-                            explorer_id,
-                        },
-                    ))
-                    .with_children(|item| {
-                        item.spawn((
-                            Text::new("No resources."),
-                            TextFont {
-                                font_size: 12.0,
-                                ..Default::default()
-                            },
-                        ));
-                    });
-            }
-        });
+        fill_dropdown(
+            &mut commands,
+            list,
+            planet_id,
+            explorer_id,
+            resources,
+            ExpButtonActions::CreateBasic,
+            "No resources.",
+            |r| format!("{r:?}"),
+        );
     }
 }
 
@@ -114,69 +58,66 @@ pub fn fill_complex_dropdown(
     explorer_status: Res<ExplorerInfoRes>,
     target_entity: Res<EntityClickRes>,
 ) {
-    for (list, dropd) in lists {
-        // don't run if the user is not looking at an explorer
-        if target_entity.explorer.is_none() || !matches!(dropd, ListType::ComplexList) {
+    let Some(explorer_id) = target_entity.explorer else {
+        return;
+    };
+
+    let planet_id = explorer_status
+        .map
+        .get_current_planet(&explorer_id)
+        .unwrap_or(0);
+
+    let planets_info = orchestrator.orchestrator.get_planets_info();
+    let single_info = planets_info.get_info(planet_id);
+
+    let empty = HashSet::new();
+
+    let resources = single_info
+        .and_then(|p| p.supported_combination.as_ref())
+        .unwrap_or(&empty);
+
+    for (list, dropd) in lists.iter() {
+        if !matches!(dropd, ListType::ComplexList) {
             continue;
         }
 
-        // clear previous table
-        commands.entity(list).despawn_children();
+        fill_dropdown(
+            &mut commands,
+            list,
+            planet_id,
+            explorer_id,
+            resources,
+            ExpButtonActions::CreateComplex,
+            "No combinations.",
+            |r| format!("{r:?}"),
+        );
+    }
+}
 
-        let explorer_id = target_entity.explorer.unwrap();
-        let planet_id = explorer_status
-            .map
-            .get_current_planet(&explorer_id)
-            .unwrap_or(0);
+fn fill_dropdown<T, FAction, FText>(
+    commands: &mut Commands,
+    list: Entity,
+    planet_id: u32,
+    explorer_id: u32,
+    resources: &HashSet<T>,
+    make_action: FAction,
+    empty_text: &str,
+    make_text: FText,
+)
+where
+    T: Copy,
+    FAction: Fn(T) -> ExpButtonActions,
+    FText: Fn(T) -> String,
+{
+    commands.entity(list).despawn_children();
 
-        //get the planet's available resources
-        let planets_info = orchestrator.orchestrator.get_planets_info();
-        let single_info = planets_info.get_info(planet_id);
-
-        let resources = match single_info {
-            Some(safe_planets) => {
-                if let Some(x) = &safe_planets.supported_combination{
-                    x
-                } else {
-                    &HashSet::new()
-                }
-            },
-            None => &HashSet::new(),
-        };
-
-        // create the actual UI element
-        commands.entity(list).with_children(|parent| {
-            if !resources.is_empty() {
-                for complex in resources {
-                    parent
-                        .spawn((
-                            Button,
-                            ExpButtonActions::CreateComplex(*complex),
-                            Node {
-                                height: Val::Px(28.0),
-                                padding: UiRect::horizontal(Val::Px(8.0)),
-                                align_items: AlignItems::Center,
-                                ..default()
-                            },
-                            DropdownItem {
-                                planet_id,
-                                explorer_id,
-                            },
-                        ))
-                        .with_children(|item| {
-                            item.spawn((
-                                Text::new(format!("{:?}", complex)),
-                                TextFont {
-                                    font_size: 14.0,
-                                    ..Default::default()
-                                },
-                            ));
-                        });
-                }
-            } else {
+    commands.entity(list).with_children(|parent| {
+        if !resources.is_empty() {
+            for resource in resources {
                 parent
                     .spawn((
                         Button,
+                        make_action(*resource),
                         Node {
                             height: Val::Px(28.0),
                             padding: UiRect::horizontal(Val::Px(8.0)),
@@ -190,16 +131,40 @@ pub fn fill_complex_dropdown(
                     ))
                     .with_children(|item| {
                         item.spawn((
-                            Text::new("No combinations."),
+                            Text::new(make_text(*resource)),
                             TextFont {
-                                font_size: 12.0,
+                                font_size: 14.0,
                                 ..Default::default()
                             },
                         ));
                     });
             }
-        });
-    }
+        } else {
+            parent
+                .spawn((
+                    Button,
+                    Node {
+                        height: Val::Px(28.0),
+                        padding: UiRect::horizontal(Val::Px(8.0)),
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    DropdownItem {
+                        planet_id,
+                        explorer_id,
+                    },
+                ))
+                .with_children(|item| {
+                    item.spawn((
+                        Text::new(empty_text),
+                        TextFont {
+                            font_size: 12.0,
+                            ..Default::default()
+                        },
+                    ));
+                });
+        }
+    });
 }
 
 /// fill the destination menu for explorers with the neighbors of the
